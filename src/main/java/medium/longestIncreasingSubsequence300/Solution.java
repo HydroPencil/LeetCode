8b278c9769346dd6a04fcb240df12d082315437d
HydroPencil <skdlghwns@gmail.com>
2024-01-05 22:50:43 +0900

    feat: binarysearch solution

--- src/main/java/medium/longestIncreasingSubsequence300/Solution.java
+++ src/main/java/medium/longestIncreasingSubsequence300/Solution.java
@@ -1,2 +1,28 @@
-package medium.longestIncreasingSubsequence300;public class Solution {
+package medium.longestIncreasingSubsequence300;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] subsequence = new int[nums.length];
+        Arrays.fill(subsequence,0);
+        int maxSize = 0;
+
+        for(int i : nums){
+            int low=0, high=maxSize;
+
+            while(low != high){
+                int mid = (low+high)/2;
+                if(subsequence[mid] < i){
+                    low = mid+1;
+                } else {
+                    high = mid;
+                }
+            }
+
+            subsequence[low] = i;
+            if(low==maxSize){
+                maxSize++;
+            }
+        }
+        return maxSize;
+    }
 }
