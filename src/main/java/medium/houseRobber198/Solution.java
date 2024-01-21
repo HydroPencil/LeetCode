471e738c7094f2ce83cd072faaeada46e833b4b3
HydroPencil <skdlghwns@gmail.com>
2024-01-21 20:27:02 +0900

    feat: dp

---
+++ src/main/java/medium/houseRobber198/Solution.java
@@ -0,0 +1,14 @@
+package medium.houseRobber198;
+
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length+1];
+        dp[1] = nums[0];
+
+        for(int i=2; i<dp.length; i++){
+            dp[i] = (dp[i-1]<dp[i-2]+nums[i-1]) ? dp[i-2]+nums[i-1] : dp[i-1];
+        }
+
+        return dp[nums.length];
+    }
+}
